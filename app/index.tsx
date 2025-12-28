6f57b4efa8b342ed80a96993ab2d4eb879a50ca1
Simon Chen <simonc8192@gmail.com>
2025-12-28 10:16:44 -0800

    - new about screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import { TestColors } from "../theme/colors";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome!</Text>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen
+      </Link>
     </View>
   );
 }
@@ -12,12 +15,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
-    fontSize: 30,
-    fontWeight: "700",
-    color: TestColors.dark[200],
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
   },
 });
