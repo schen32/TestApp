c8ed190ae61538b9641cd917cdb3a96e1cafbd82
Simon Chen <simonc8192@gmail.com>
2025-12-28 08:52:51 -0800

    - defined colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { TestColors } from "../theme/colors";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome</Text>
+      <Text style={styles.text}>Welcome!</Text>
     </View>
   );
 }
@@ -11,11 +12,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
-    color: "#fff",
+    fontSize: 30,
+    fontWeight: "700",
+    color: TestColors.dark[200],
   },
 });
