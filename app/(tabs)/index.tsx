5b9cf6b6138820f759e2196238be8153bcffb660
Simon Chen <simonc8192@gmail.com>
2025-12-30 14:15:24 -0800

    - display picked emoji sticker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
 import EmojiList from "@/components/EmojiList";
 import EmojiPicker from "@/components/EmojiPicker";
+import EmojiSticker from "@/components/EmojiSticker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from "expo-image-picker";
@@ -56,6 +57,12 @@ export default function Index() {
           imgSource={PlaceholderImage}
           selectedImage={selectedImage}
         ></ImageViewer>
+        {pickedEmoji && (
+          <EmojiSticker
+            imageSize={40}
+            stickerSource={pickedEmoji}
+          ></EmojiSticker>
+        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
