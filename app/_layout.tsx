1ac62b86ae38b9d359062a6cf3293463309b019e
Simon Chen <simonc8192@gmail.com>
2025-12-31 16:58:47 -0800

    - new icon - show status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+      </Stack>
+      <StatusBar style="light"></StatusBar>
+    </>
   );
 }
